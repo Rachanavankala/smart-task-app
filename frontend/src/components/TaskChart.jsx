f58847679bc593a8cecd54df97451b6f7fc109dc
Rachana Vankala <rachanav9727@gmail.com>
2025-07-03 14:53:34 +0530

    change

--- frontend/src/components/TaskChart.jsx
+++ frontend/src/components/TaskChart.jsx
@@ -1,6 +1,6 @@
 // frontend/src/components/TaskChart.jsx
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
